5946f57ddb8aef812630b751f9136bac92aa9f1e
Juan Francisco Gato Luis <jfcogato@gmail.com>
2014-04-08 16:25:07 +0200

    Update with clickable icons

--- src/com/example/moleurlshortener/MainActivity.java
+++ src/com/example/moleurlshortener/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.moleurlshortener;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,32 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		final ImageView googleButton = (ImageView) this
+				.findViewById(R.id.google_button);
+		final ImageView moleButton = (ImageView) this
+				.findViewById(R.id.mole_button);
+		final ImageButton sendButton = (ImageButton) this
+				.findViewById(R.id.send_button);
+
+		googleButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				moleButton.setImageResource(R.drawable.ic_mole_grey);
+				googleButton.setImageResource(R.drawable.ic_google);
+			}
+		});
+
+		moleButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				moleButton.setImageResource(R.drawable.ic_mole);
+				googleButton.setImageResource(R.drawable.ic_google_grey);
+			}
+		});
+
 	}
 
 	@Override
